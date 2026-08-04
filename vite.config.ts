7d90e2296075290cd0cf13232bd51ad8f749c603
Codego <101083976+kitenebie@users.noreply.github.com>
2026-08-04 11:09:50 +0800

    Use official GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
-    base: "/",
+    base: "/OSCA/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
